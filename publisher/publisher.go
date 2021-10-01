46abed7dd52178b9106f5254e9be95cf4e2b5210
daniilsolovey <sda010394@gmail.com>
2021-10-01 23:10:23 +0300

    add several subscribers

--- publisher/publisher.go
+++ publisher/publisher.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"strconv"
+	"sync"
 	"time"
 
 	"github.com/nats-io/nats"
@@ -9,6 +11,10 @@ import (
 
 type Request struct {
 	ID int
+}
+
+type Request_3 struct {
+	Name string
 }
 
 func main() {
@@ -29,12 +35,33 @@ func main() {
 	chanForSend := make(chan *Request)
 	encodedConn.BindSendChan("request_subject", chanForSend)
 
-	i := 0
-	for {
-		request := Request{ID: i}
-		log.Infof(nil, "sending request, request_id: %d", request.ID)
-		chanForSend <- &request
-		time.Sleep(time.Second * 1)
-		i = i + 1
-	}
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		i := 0
+		for {
+			request := Request{ID: i}
+			log.Infof(nil, "sending request, request_id: %d", request.ID)
+			chanForSend <- &request
+			time.Sleep(time.Second * 1)
+			i = i + 1
+		}
+	}()
+
+	chanForSend_3 := make(chan *Request_3)
+	encodedConn.BindSendChan("request_subject_3", chanForSend_3)
+
+	wg.Add(1)
+	go func() {
+		i := 0
+		for {
+			request_3 := Request_3{Name: "testName_xxxx: " + strconv.Itoa(i)}
+			log.Infof(nil, "sending request to subscriber_3, request_id: %s", request_3.Name)
+			chanForSend_3 <- &request_3
+			time.Sleep(time.Second * 1)
+			i = i + 1
+		}
+	}()
+
+	wg.Wait()
 }
--- subscriber/subscriber.go
+++ subscriber/subscriber.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"sync"
+
 	"github.com/nats-io/nats"
 	"github.com/reconquest/pkg/log"
 )
 
 type Request struct {
 	ID int
+}
+
+type Request_3 struct {
+	Name string
 }
 
 func main() {
@@ -24,11 +30,40 @@ func main() {
 
 	log.Info("connected to nats and ready to receive messages")
 
-	chanForReceive := make(chan *Request)
-	encodedConn.BindRecvChan("request_subject", chanForReceive)
+	var wg sync.WaitGroup
+	wg.Add(1)
+	chanForReceive_1 := make(chan *Request)
+	encodedConn.BindRecvChan("request_subject", chanForReceive_1)
+	go func() {
+		for {
+			request := <-chanForReceive_1
+			log.Infof(nil, "received request for subscriber_1, request_id: %d", request.ID)
+		}
 
-	for {
-		request := <-chanForReceive
-		log.Infof(nil, "received request, request_id: %d", request.ID)
-	}
+	}()
+
+	wg.Add(1)
+	chanForReceive_2 := make(chan *Request)
+	encodedConn.BindRecvChan("request_subject", chanForReceive_2)
+	go func() {
+		for {
+			request := <-chanForReceive_2
+			log.Infof(nil, "received request for subscriber_2, request_id: %d", request.ID)
+		}
+
+	}()
+
+	wg.Add(1)
+	chanForReceive_3 := make(chan *Request_3)
+	encodedConn.BindRecvChan("request_subject_3", chanForReceive_3)
+
+	go func() {
+		for {
+			request := <-chanForReceive_3
+			log.Infof(nil, "received request for subscriber_2, request_id: %s", request.Name)
+		}
+
+	}()
+
+	wg.Wait()
 }
